8529e183635d0f76fc101944fa438e007e568eaf
jadyn-h <jadynmharrison@gmail.com>
2026-06-29 01:22:25 -0400

    created the clicker game
    still gotta figure out some errors with connecting the minigames to the main game

--- Scenes/minigame_1.gd
+++ Scenes/minigame_1.gd
@@ -17,15 +17,14 @@ func _process(delta: float) -> void:
 	
 	if cheese_collected == 3:
 		if Global.minigames_done > 3:
-			get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
-			#MUST CHANGE TO AN END SCREEN LATER
+			get_tree().change_scene_to_file("res://Scenes/done_screen.tscn")
 		else:
-			get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+			get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
 	
 	if timer_end:
 		Global.minigames_done -=1
 		Global.lives -= 1
-		get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+		get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
 		
 
 func _on_cheese_collection() -> void:
